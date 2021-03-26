d2224d3454012ee3d3a9179353d624849650baf6
Vladislav <vlad.tiw27@gmail.com>
2021-03-26 11:42:35 +0200

    написал фиксацию фильтра возле хедера при нажатии

--- src/js/main.js
+++ src/js/main.js
@@ -10,22 +10,29 @@ window.addEventListener('DOMContentLoaded', () => {
           clickable: true,
         }
       });
-
+      
+      // Фиксируем хедер
       function fixedHeader() {
         const menu = document.querySelector('.header__menu'),
               link = document.querySelector('.header__link'),
-              header = document.querySelector('.header');
+              header = document.querySelector('.header'),
+              filter = document.querySelector('.header__filter');
 
         let lastPos = 0;
 
         window.addEventListener('scroll', () => {
           if (pageYOffset < lastPos && pageYOffset < 200) {
+            if (pageYOffset < 800) {
+              filter.classList.remove('header__filter_active');
+            }
             menu.classList.remove('header__menu_active');
             link.classList.remove('header__link_active');
             header.classList.remove('header_active');
           } else if (pageYOffset > lastPos && pageYOffset > 200) {
+            if (pageYOffset > 800) {
+              filter.classList.add('header__filter_active');
+            }
             menu.classList.add('header__menu_active');
-            link.classList.add('header__link_active');
             if (header.classList.contains('header_active')) {
               header.classList.remove('header_active');
             }
@@ -37,6 +44,64 @@ window.addEventListener('DOMContentLoaded', () => {
         })
       }
       fixedHeader();
+
+      // Фиксируем фильтр
+      function fixedFilter() {
+        const btn = document.querySelector('.header__filter'),
+              filter = document.querySelector('.filter'),
+              menu = document.querySelector('.header__menu'),
+              header = document.querySelector('.header'),
+              slider = document.querySelector('.slider');
+    
+       
+        let headerHeight = header.clientHeight;
+        let menuHeight = menu.clientHeight;
+        let filterHeight = filter.clientHeight;
+        
+        window.addEventListener('scroll', () => {
+            filter.classList.remove('filter_active');
+        });
+
+        btn.addEventListener('click', (e) => {
+            e.preventDefault();
+            if (header.classList.contains('header_active')) {
+              if (!filter.classList.contains('filter_active')) {
+                filterAdd(headerHeight, 5);
+              } else {
+                filterRemove();
+              }
+
+            }
+            if (menu.classList.contains('header__menu_active')) {
+              if (!filter.classList.contains('filter_active')) {
+                filterAdd(menuHeight, -12);
+              } else {
+                filterRemove();
+              }
+            }
+        });
+
+        function filterAdd(elemHeight,addPx) {
+          filter.classList.add('filter_active');
+          filter.style.top = `${elemHeight - addPx}px`;
+
+          const div = document.createElement('div');
+          div.style.height = filterHeight + 'px';
+          div.classList.add('filter__divider');
+          slider.insertAdjacentElement('afterend', div);
+        }
+
+        function filterRemove() {
+          filter.classList.remove('filter_active');
+          filter.style.top = `${0}px`;
+
+          document.querySelectorAll('.filter__divider').forEach(item => {
+            item.remove();
+          })
+        }
+      }
+
+      fixedFilter();
 
     //Тестовый ховер скрипт для карточек, пока что не определился как лучше выполнить этот эффект.
     // function cardHover() {
